cf007add44c2b53a3244f5374629c7442414c8db
Saifisco <isco7.official@gmail.com>
2025-08-31 23:13:20 -0700

    fix bug in CartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -25,7 +25,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleDecrement = (item) => {
-    if (item.quantity > 0) {
+    if (item.quantity > 1) {
       dispatch(
         updateQuantity({ name: item.name, quantity: item.quantity - 1 })
       );
